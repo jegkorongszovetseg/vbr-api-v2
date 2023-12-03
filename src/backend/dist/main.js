b755be91a02c3985416622ffa0f7f780bad57c61
Akos Stegner <a.stegner@mito.hu>
2023-12-03 10:09:12 +0100

    feat: Add ts for BE

---
+++ src/backend/dist/main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function sayMyName(name) {
+    if (name === "Heisenberg") {
+        console.log("You're right 👍");
+    }
+    else {
+        console.log("You're wrong 👎");
+    }
+}
+sayMyName("Heisenberg");
