1c85d5d35c2f73456e5ae75bb12d910e1277ec01
Akos Stegner <a.stegner@mito.hu>
2024-01-22 08:33:58 +0100

    Add middleware

--- packages/middlewares/validation.ts
+++ packages/middlewares/validation.ts
@@ -1,3 +1,9 @@
-export function apiValidation() {
-  
+export async function apiValidation() {
+  const result = await readValidatedBody(event, body => userSchema.safeParse(body).data) // or `.parse` to directly throw an error
+
+  if (!result.success)
+    throw result.error.issues
+
+  // User object is validated and typed!
+  return result.data
 }
